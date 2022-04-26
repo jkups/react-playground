e26b44c494e206c2ddb627f740b868d6f1ce2b52
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-04-26 20:41:46 +1000

    Basic Working TodoList

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   mode: "jit",
   content: [
+<<<<<<< HEAD
     "./src/**/*.{js,jsx,ts,tsx}"
+=======
+    "./src/**/*.{js,jsx,ts,tsx}",
+>>>>>>> 639e1c5 (Basic Working TodoList)
   ],
   theme: {
     extend: {},
