a9042d3d8affaff70d1424f1aa46e61bdabe238c
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-04-27 10:57:42 +1000

    Refactor with Reducer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   mode: "jit",
   content: [
-<<<<<<< HEAD
     "./src/**/*.{js,jsx,ts,tsx}"
-=======
-    "./src/**/*.{js,jsx,ts,tsx}",
->>>>>>> 639e1c5 (Basic Working TodoList)
   ],
   theme: {
     extend: {},
